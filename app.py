732ee1b441fba26d04f5d4500ed3f5b2c6f13249
sol4nki <solankipranjal44@gmail.com>
2026-08-02 14:53:08 +0530

    a bit more secure

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import render_template
 # from flask import url_for
 
 import base64
-import json
 
 
 app = Flask(__name__)
@@ -15,12 +14,14 @@ def check_cookie():
         return cookie
     return False
 
+    
+
 @app.route("/")
 def hello_world():
     name = request.cookies.get('name')
     username = request.cookies.get('username')
     branch = request.cookies.get('branch')
-
+    # store device id in database
     if name and username and branch:
         return render_template('home.html', name = name, username=username, branch=branch)
     return render_template('index.html')
